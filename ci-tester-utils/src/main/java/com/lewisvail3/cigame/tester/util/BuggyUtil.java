4c8a80d07bca3ffc1de535d8bbf210cb2362103f
Lewis Vail <lewisvail3@gmail.com>
2016-02-26 23:20:32 -0500

    Initial checkin of ci-game-tester.

---
+++ ci-tester-utils/src/main/java/com/lewisvail3/cigame/tester/util/BuggyUtil.java
@@ -0,0 +1,13 @@
+package com.lewisvail3.cigame.tester.util;
+
+/**
+ * This is a utility class that will be full of bugs that will affect my ci-game score
+ * @author lewisvail
+ *
+ */
+public class BuggyUtil {
+
+	public void foo() {
+		System.out.println("Foo on you!");
+	}
+}
