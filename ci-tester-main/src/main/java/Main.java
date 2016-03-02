e280cc8e87482c8365ee47e2c323b8ca550edda9
Lewis Vail <lewisvail3@gmail.com>
2016-03-02 00:04:46 -0500

    Updated Main to test incremental FindBugs builds.

--- ci-tester-main/src/main/java/Main.java
+++ ci-tester-main/src/main/java/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("This is my main method.  It doesn't do much...");
+		System.out.println("Needed to change this file");
 	}
 
 }
