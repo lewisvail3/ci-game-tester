ec169b21423d9b527176976033089e1e3d22be1f
Lewis Vail <lewisvail3@gmail.com>
2016-03-01 23:20:33 -0500

    failing test.

--- ci-tester-main/src/test/java/MainTest.java
+++ ci-tester-main/src/test/java/MainTest.java
@@ -1,12 +1,14 @@
+import static org.junit.Assert.fail;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class MainTest {
 
-	@Ignore
 	@Test
 	public void testMain() {
 		Main.main(new String[]{"arg1"});
+		fail("Testing failed test");
 	}
 
 }
